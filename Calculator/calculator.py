5c705e1bd5d5697db496eda633c8dddf26819d5c
Gabrielkozlowski <gabriel89krk@gmail.com>
2022-12-17 16:19:29 +0100

    adding function to buttons

--- Calculator/calculator.py
+++ Calculator/calculator.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from math import sqrt
 
 root = Tk()
 root.title('Calculator')
@@ -18,50 +19,107 @@ sum_result = Entry(root, width=50)
 sum_result.grid(row=1, column=0, columnspan=4)
 
 
-def button_click(number):
+def ButtoClick(number):
     current = sum_result.get()
     sum_result.delete(0, END)
     sum_result.insert(0, str(current) + str(number))
 
 
-def button_delete():
+def ButtonDelete():
     sum_result.delete(0, END)
 
 
-def button_undo():
+def ButtonUndo():
     current = sum_result.get()
     sum_result.delete(len(current)-1)
 
+def ButtonNegative():
+    current = float(sum_result.get())
+    if current == abs(current):
+        sum_result.delete(0, END)
+        sum_result.insert(0, -abs(current)) 
+    else:
+        current = abs(current)
+        sum_result.delete(0, END)
+        sum_result.insert(0, abs(current))
 
-division_button = Button(root, width=11, pady=15, text='%', command=lambda: button_click)
-ce_button = Button(root, width=11, pady=15, text='CE', command=lambda: button_delete())
-c_button = Button(root, width=11, pady=15, text='C', command=lambda: button_delete())
-back_space_button = Button(root, width=11, pady=15, text='<--', command=lambda: button_undo())
 
-one_division_by = Button(root, width=11, pady=15, text='1/x', command=lambda: button_click)
-power_by_two = Button(root, width=11, pady=15, text='x**2', command=lambda: button_click)
-square_root = Button(root, width=11, pady=15, text='square_root', command=lambda: button_click)
-division = Button(root, width=11, pady=15, text='/', command=lambda: button_click)
+def ButtonDot():
+    current = sum_result.get()
+    if current == '':
+        pass
+    else:
+        if '.' in current:
+            pass
+        else:
+            sum_result.delete(0, END)
+            sum_result.insert(0, current + '.')
 
-multiplication_button = Button(root, width=11, pady=15, text='X', command=lambda: button_click)
-nine_button = Button(root, width=11, pady=15, text='9', command=lambda: button_click(9))
-eight_button = Button(root, width=11, pady=15, text='8', command=lambda: button_click(8))
-seven_button = Button(root, width=11, pady=15, text='7', command=lambda: button_click(7))
 
-subtraction_button = Button(root, width=11, pady=15, text='-', command=lambda: button_click)
-six_button = Button(root, width=11, pady=15, text='6', command=lambda: button_click(6))
-five_button = Button(root, width=11, pady=15, text='5', command=lambda: button_click(5))
-four_button = Button(root, width=11, pady=15, text='4', command=lambda: button_click(4))
+def ButtonOneDivisionBy():
+    current = sum_result.get()
+    current = 1 / float(current)
+    sum_result.delete(0, END)
+    sum_result.insert(0, current)
 
-addition_button = Button(root, width=11, pady=15, text='+', command=lambda: button_click)
-three_button = Button(root, width=11, pady=15, text='3', command=lambda: button_click(3))
-two_button = Button(root, width=11, pady=15, text='2', command=lambda: button_click(2))
-one_button = Button(root, width=11, pady=15, text='1', command=lambda: button_click(1))
+def ButtoPowerByTwo():
+    current = sum_result.get()
+    if '.' in current:
+        current = float(current)
+        sum_result.delete(0, END)
+        sum_result.insert(0, current ** 2)
+    else:
+        current = int(current)
+        sum_result.delete(0, END)
+        sum_result.insert(0, current ** 2)
 
-sum_button = Button(root, width=11, pady=15, text='=', command=lambda: button_click)
-comma_button = Button(root, width=11, pady=15, text=',', command=lambda: button_click)
-zero_button = Button(root, width=11, pady=15, text='0', command=lambda: button_click(0))
-naegative_button = Button(root, width=11, pady=15, text='+/-', command=lambda: button_click)
+
+def ButtoSqrt():
+    current = sum_result.get()
+    if '.' in current:
+        current = float(current)
+        sum_result.delete(0, END)
+        sum_result.insert(0, sqrt(current))
+    else:
+        current = int(current)
+        result = sqrt(current)
+        if result.is_integer():
+            sum_result.delete(0, END)
+            sum_result.insert(0, int(result))
+        else:
+            sum_result.delete(0, END)
+            sum_result.insert(0, result)
+        
+
+division_button = Button(root, width=11, pady=15, text='%', command=lambda: ButtoClick)
+ce_button = Button(root, width=11, pady=15, text='CE', command=lambda: ButtonDelete())
+c_button = Button(root, width=11, pady=15, text='C', command=lambda: ButtonDelete())
+back_space_button = Button(root, width=11, pady=15, text='<--', command=lambda: ButtonUndo())
+
+one_division_by = Button(root, width=11, pady=15, text='1/x', command=lambda: ButtonOneDivisionBy())
+power_by_two = Button(root, width=11, pady=15, text='x**2', command=lambda: ButtoPowerByTwo())
+square_root = Button(root, width=11, pady=15, text='square_root', command=lambda: ButtoSqrt())
+division = Button(root, width=11, pady=15, text='/', command=lambda: ButtoClick)
+
+multiplication_button = Button(root, width=11, pady=15, text='X', command=lambda: ButtoClick)
+nine_button = Button(root, width=11, pady=15, text='9', command=lambda: ButtoClick(9))
+eight_button = Button(root, width=11, pady=15, text='8', command=lambda: ButtoClick(8))
+seven_button = Button(root, width=11, pady=15, text='7', command=lambda: ButtoClick(7))
+
+subtraction_button = Button(root, width=11, pady=15, text='-', command=lambda: ButtoClick)
+six_button = Button(root, width=11, pady=15, text='6', command=lambda: ButtoClick(6))
+five_button = Button(root, width=11, pady=15, text='5', command=lambda: ButtoClick(5))
+four_button = Button(root, width=11, pady=15, text='4', command=lambda: ButtoClick(4))
+
+addition_button = Button(root, width=11, pady=15, text='+', command=lambda: ButtoClick)
+three_button = Button(root, width=11, pady=15, text='3', command=lambda: ButtoClick(3))
+two_button = Button(root, width=11, pady=15, text='2', command=lambda: ButtoClick(2))
+one_button = Button(root, width=11, pady=15, text='1', command=lambda: ButtoClick(1))
+
+sum_button = Button(root, width=11, pady=15, text='=', command=lambda: ButtoClick)
+comma_button = Button(root, width=11, pady=15, text='.', command=lambda: ButtonDot())
+zero_button = Button(root, width=11, pady=15, text='0', command=lambda: ButtoClick(0))
+naegative_button = Button(root, width=11, pady=15, text='+/-', command=lambda: ButtonNegative())
 
 
 
